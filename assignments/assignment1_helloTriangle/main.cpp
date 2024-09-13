9535db75c6207f349d572bc3a3df61d299680fed
cocoatehcat <147646449+cocoatehcat@users.noreply.github.com>
2024-09-13 11:09:22 -0400

    End of class work, the triangle is several different colors and is moving. Before class refactor

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -10,28 +10,40 @@ const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	// X	 Y	   Z	 R	   G	B	  A
+	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+	 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
 };
 
 // Compiling a shader
 const char* vertexShaderSource = R"(
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 aColor;
+out vec4 Color; //Varying
+uniform float uTime;
 void main()
 {
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+   Color = aColor; // Pass-through
+   vec3 pos = aPos;
+   pos.y += sin(uTime * 4.0 + pos.x) / 4.0;
+   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
 }
 )";
 
+// Fragment Shader
 const char* fragmentShaderSource = R"(
 #version 330 core
 out vec4 FragColor;
+in vec4 Color;
+uniform float uTime;
+uniform vec4 uColor = vec4(1.0);
 
 void main()
 {
-    FragColor = vec4(1.0f, 0.2f, 0.7f, 1.0f);
+    FragColor = Color * (sin(uTime) * 0.5 + 0.5); 
+	//This is the hard code: vec4(1.0f, 0.2f, 0.7f, 1.0f);
 } 
 )";
 
@@ -64,9 +76,13 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// How to interpret Vertex Data
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Position for XYZ
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// Color RGBA
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(sizeof(float) * 3));
+	glEnableVertexAttribArray(1);
 
 	// Unbind Buffer
 	//glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -122,15 +138,21 @@ int main() {
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+		// Counting number of seconds passed since the beginning
+		float time = (float)glfwGetTime();
+
 		//Clear framebuffer
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Knows what program to use
 		glUseProgram(shaderProgram);
+		int timeLoc = glGetUniformLocation(shaderProgram, "uTime");
+		glUniform1f(timeLoc, time);
 
 		// Binding Vertex aray and draw it
 		glBindVertexArray(VAO);
+		// Draw Call
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//Drawing happens here!
