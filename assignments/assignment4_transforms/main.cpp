2d1f08680dc6c981509e64f56cf2a515ae5ebedc
cocoatehcat <147646449+cocoatehcat@users.noreply.github.com>
2024-10-21 22:56:00 -0400

    Implemented the first bit of the camera class, it is rotating, so that's good.

--- assignments/assignment4_transforms/main.cpp
+++ assignments/assignment4_transforms/main.cpp
@@ -248,19 +248,23 @@ int main() {
 
 		// Making the view
 		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // moving scene back to make it seems right to us
+		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // moving scene back to make it seems right to us
+		float radius = 10.0f;
+		float camX = static_cast<float>(sin(glfwGetTime()) * radius);
+		float camZ = static_cast<float>(cos(glfwGetTime()) * radius);
+		view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 		// Projection
 		glm::mat4 proj = glm::mat4(1.0f);
 		proj = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
 		// Retrieve uniform locations
-		unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+		//unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
 		unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
 		unsigned int projLoc = glGetUniformLocation(shaderProgram, "proj");
 
 		//pass to shaders
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 
@@ -272,9 +276,9 @@ int main() {
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//glDrawArrays(GL_TRIANGLES, 0, 36);
 
-		for (unsigned int i = 0; i < 20; i++) {
+		for (unsigned int i = 0; i < 10; i++) { //change to 20 later
 			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, createPos());
+			model = glm::translate(model, cubePositions[i]);
 			float angle = 20.0f * i;
 			model = glm::rotate(model, glm::radians(angle) * time, createPos());
 			model = glm::scale(model, createPos());
